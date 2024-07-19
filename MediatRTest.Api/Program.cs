5cdb30db043642ba0697b7e7a51d0a78ee063473
kubagdynia <kubagdynia@gmail.com>
2024-07-19 12:45:53 +0200

    Added API project structure

---
+++ MediatRTest.Api/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", false, false)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, false)
+    .AddCommandLine(args)
+    .AddEnvironmentVariables();
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddProblemDetails();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseExceptionHandler();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
